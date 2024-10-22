ff60aa2060b1084f8f340da0c0284bdd1ff08a6c
Ridwan <ridwanpr@outlook.com>
2024-10-22 07:41:41 +0700

    add select category

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
 
-class Meals extends StatelessWidget {
-  const Meals({super.key, required this.title, required this.meals});
+class MealScreen extends StatelessWidget {
+  const MealScreen({super.key, required this.title, required this.meals});
 
   final String title;
   final List<Meal> meals;
 
   @override
   Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh ... nothing here!',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Text('Meal Screen'),
+      body: content,
     );
   }
 }
